857e2964621d1708e9ddd014f4986ebc6b0e11ad
erdisonmalko <erdisonmalko@gmail.com>
2026-03-14 13:17:59 +0100

    testing pull from railway deployment

---
+++ tests/test_railway_auth.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import requests
+
+headers = {
+    'Authorization': f'Bearer {os.getenv("RAILWAY_TOKEN")}',
+    'Content-Type': 'application/json'
+}
+
+query = """
+{
+  me {
+    id
+    email
+  }
+}
+"""
+
+response = requests.post(
+    'https://backboard.railway.app/graphql/v2',  # updated URL
+    json={'query': query},
+    headers=headers
+)
+print(response.status_code)
+print(response.text)
