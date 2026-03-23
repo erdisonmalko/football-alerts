9cefac745695b38a44a35484ef8322b36ce5a8d4
erdisonmalko <erdisonmalko@gmail.com>
2026-03-23 22:12:31 +0100

    fixed env file

--- alembic/env.py
+++ alembic/env.py
@@ -38,15 +38,11 @@ def do_run_migrations(connection):
 
 
 async def run_async_migrations() -> None:
-    if settings.is_production:
-        db_url = settings.DATABASE_URL
-    else:
-        db_url = settings.LOCAL_DATABASE_URL
-
-    engine = create_async_engine(db_url, poolclass=pool.NullPool)
+    engine = create_async_engine(settings.DATABASE_URL, poolclass=pool.NullPool)
     async with engine.connect() as connection:
         await connection.run_sync(do_run_migrations)
     await engine.dispose()
+
 
 def run_migrations_online() -> None:
     asyncio.run(run_async_migrations())
--- app/v1/core/config.py
+++ app/v1/core/config.py
@@ -13,9 +13,6 @@ class Settings(BaseSettings):
 
     # Database
     DATABASE_URL: str = "postgresql+asyncpg://postgres:password@db:5432/football_alerts"
-    LOCAL_DATABASE_URL: str = (
-        "postgresql+asyncpg://postgres:password@localhost:5432/football_alerts"
-    )
     DATABASE_URL_SYNC: str = "postgresql://postgres:password@db:5432/football_alerts"
 
     # Redis
